687314ac8ea281f6718b81e26b4e4f827eb08ed4
Boaz Yaniv <boaz.y@taboola.com>
2017-01-11 11:18:09 +0200

    tables controller

---
+++ src/main/java/TablesController.java
@@ -0,0 +1,23 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by boaz.y on 11/01/2017.
+ */
+@Controller
+@EnableAutoConfiguration
+public class TablesController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(TablesController.class, args);
+    }
+}
