93a818facbe1200800834397988042d5840a52a6
Eyal Segal <eyal.s@taboola.com>
2017-01-11 14:01:32 +0200

    -- Added google client id

--- src/main/java/com/taboola/tables/config/TablesConfig.java
+++ src/main/java/com/taboola/tables/config/TablesConfig.java
@@ -1,19 +1,25 @@
 package com.taboola.tables.config;
 
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
 
 /**
  * Created by eyal.s on 11/01/2017.
  */
 @Configuration
 public class TablesConfig {
-    /*@Bean
+    @Bean
     public GoogleIdTokenVerifier googleTokenVerifier (){
 
         return new GoogleIdTokenVerifier.Builder(new NetHttpTransport.Builder().build(), new JacksonFactory())
                 .setAudience(Collections.singletonList("78184266687-jl0emgs5mqnlo0ivo5trn0q93bmq2els.apps.googleusercontent.com"))
                 .build();
-    }*/
+    }
 
 }
