04e644dc9bc69d600437e740fb1bf7c8fbc8724f
Shiran Schwartz <shiran.s@taboola.com>
2017-01-11 10:19:34 +0200

    add pom an web

---
+++ src/main/java/web/ServerExample.java
@@ -0,0 +1,19 @@
+package springboot;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@SpringBootApplication
+public class Example {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+
+}
