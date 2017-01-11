acebd68d372ad55064bc55b45d8d0e466e99036b
Rami Stern <rami.s@taboola.com>
2017-01-11 16:36:41 +0200

    Moved login wiring to home page

---
+++ src/main/java/com/taboola/tables/entities/LoginResult.java
@@ -0,0 +1,17 @@
+package com.taboola.tables.entities;
+
+/**
+ * Created by rami.s on 1/11/17.
+ */
+public class LoginResult {
+    private boolean loginSuccess;
+
+    public boolean isLoginSuccess() {
+        return loginSuccess;
+    }
+
+    public void setLoginSuccess(boolean loginSuccess) {
+        this.loginSuccess = loginSuccess;
+    }
+
+}
