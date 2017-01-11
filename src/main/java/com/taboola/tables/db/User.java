0927d62deb947b1918ae1d1d5b72a3cc8b66cfed
Rami Stern <rami.s@taboola.com>
2017-01-11 19:00:40 +0200

    Separate between gmail id and gmail mail address

--- src/main/java/com/taboola/tables/db/User.java
+++ src/main/java/com/taboola/tables/db/User.java
@@ -26,12 +26,15 @@ public class User {
     @Id
     private Long id;
     private String gmailId;
+    private String mail;
     private String name;
     private Double score = NO_SCORE;
     private String tid = NO_TID;
     private Date creationDate;
     private Date updateDate;
     private long nextAppointmentId;
+    private String picture;
+
     @ManyToMany(mappedBy = "users")
     @JsonIgnore
     private List<Appointment> appointments;
@@ -42,17 +45,19 @@ public class User {
         this.updateDate = now;
     }
 
-    public User(String name, String gmailId) {
+    public User(String name, String gmailId, String mail) {
         this();
         this.name = name;
         this.gmailId = gmailId;
+        this.mail = mail;
         this.picture = DEFAULT_PIC;
     }
 
-    public User(String name, String email, String picture) {
+    public User(String name, String gmailId, String mail, String picture) {
         this();
         this.name = name;
-        this.gmailId = email;
+        this.gmailId = gmailId;
+        this.mail = mail;
         this.picture = picture;
     }
 
@@ -69,7 +74,13 @@ public class User {
         return this.picture;
     }
 
-    private String picture;
+    public String getMail(){
+        return this.mail;
+    }
+
+    public void setMail(String mail){
+        this.mail = mail;
+    }
 
     public Date getCreationDate() {
         return creationDate;
@@ -142,6 +153,7 @@ public class User {
         return "User{" +
                 "id=" + id +
                 ", gmailId='" + gmailId + '\'' +
+                ", mail='" + mail + '\'' +
                 ", name='" + name + '\'' +
                 ", score=" + score +
                 ", tid='" + tid + '\'' +
@@ -165,6 +177,7 @@ public class User {
         if (name != null ? !name.equals(user.name) : user.name != null) return false;
         if (score != null ? !score.equals(user.score) : user.score != null) return false;
         if (tid != null ? !tid.equals(user.tid) : user.tid != null) return false;
+        if (mail != null ? !mail.equals(user.mail) : user.mail != null) return false;
         if (creationDate != null ? !creationDate.equals(user.creationDate) : user.creationDate != null) return false;
         if (updateDate != null ? !updateDate.equals(user.updateDate) : user.updateDate != null) return false;
         if (appointments != null ? !appointments.equals(user.appointments) : user.appointments != null) return false;
@@ -176,6 +189,7 @@ public class User {
     public int hashCode() {
         int result = id != null ? id.hashCode() : 0;
         result = 31 * result + (gmailId != null ? gmailId.hashCode() : 0);
+        result = 31 * result + (mail != null ? mail.hashCode() : 0);
         result = 31 * result + (name != null ? name.hashCode() : 0);
         result = 31 * result + (score != null ? score.hashCode() : 0);
         result = 31 * result + (tid != null ? tid.hashCode() : 0);
