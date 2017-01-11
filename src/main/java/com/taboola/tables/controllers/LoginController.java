7a9bac1c4221bed9de02693f133485de90ff807d
Eyal Segal <eyal.s@taboola.com>
2017-01-11 15:11:21 +0200

    -- More login stuff

---
+++ src/main/java/com/taboola/tables/controllers/LoginController.java
@@ -0,0 +1,48 @@
+package com.taboola.tables.controllers;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+/**
+ * Created by eyal.s on 11/01/2017.
+ */
+@Controller
+public class LoginController {
+
+    @Autowired
+    GoogleIdTokenVerifier googleTokenVerifier;
+
+    @RequestMapping("/login")
+    public void login(@RequestParam(name = "token") String token) throws GeneralSecurityException, IOException {
+        GoogleIdToken idToken = googleTokenVerifier.verify(token);
+        if (idToken != null) {
+            GoogleIdToken.Payload payload = idToken.getPayload();
+
+            // Print user identifier
+            String userId = payload.getSubject();
+            System.out.println("User ID: " + userId);
+
+            // Get profile information from payload
+            String email = payload.getEmail();
+            boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+            String name = (String) payload.get("name");
+            String pictureUrl = (String) payload.get("picture");
+            String locale = (String) payload.get("locale");
+            String familyName = (String) payload.get("family_name");
+            String givenName = (String) payload.get("given_name");
+
+            // Use or store profile information
+            // ...
+
+        } else {
+            System.out.println("Invalid ID token.");
+        }
+    }
+}
