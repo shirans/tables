5d01a2d86e33cd82edb1e81069c85813057e9183
Boaz Yaniv <boaz.y@taboola.com>
2017-01-12 01:52:28 +0200

    udd

---
+++ src/main/java/com/taboola/tables/db/UserDataDirectoryRepo.java
@@ -0,0 +1,13 @@
+package com.taboola.tables.db;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by boaz.y on 12/01/2017.
+ */
+@Repository
+public interface UserDataDirectoryRepo extends CrudRepository<UserDataDirectory, Long> {
+
+    UserDataDirectory findBySegmentId(String segmentId);
+}
