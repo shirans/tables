b057093ad9f48d3c67070431f8da1e95ddada407
Boaz Yaniv <boaz.y@taboola.com>
2017-01-11 12:41:17 +0200

    get appointments

---
+++ src/main/java/Appointment.java
@@ -0,0 +1,33 @@
+/**
+ * Created by boaz.y on 11/01/2017.
+ */
+public class Appointment {
+
+    private final long id;
+    private final String userName;
+    private final String location;
+    private final long appointmentTimestamp;
+
+    public Appointment(long id, String userName, String location, long appointmentTimestamp) {
+        this.id = id;
+        this.userName = userName;
+        this.location = location;
+        this.appointmentTimestamp = appointmentTimestamp;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public long getAppointmentTimestamp() {
+        return appointmentTimestamp;
+    }
+}
