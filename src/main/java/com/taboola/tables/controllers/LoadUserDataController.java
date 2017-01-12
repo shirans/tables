eb377dadbb601a96897425bae164d87d9744534c
Shiran Schwartz <shiran.s@taboola.com>
2017-01-12 04:55:04 +0200

    do not remove duplicates of userData

--- src/main/java/com/taboola/tables/controllers/LoadUserDataController.java
+++ src/main/java/com/taboola/tables/controllers/LoadUserDataController.java
@@ -2,6 +2,7 @@ package com.taboola.tables.controllers;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.h2.tools.Csv;
 import org.slf4j.Logger;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.api.client.repackaged.com.google.common.base.Strings;
+import com.google.api.client.util.Lists;
 import com.taboola.tables.db.UserSegmentData;
 import com.taboola.tables.db.UserSegmentRepo;
 
@@ -32,9 +34,11 @@ public class LoadUserDataController {
 
         userSegmentRepo.deleteAll();;
 
+        int count= 0;
         try {
             ResultSet rs = new Csv().read(path, null, null);
             while (rs.next()) {
+                count ++;
                 String tid = rs.getString(1);
                 String liverampSegment = rs.getString(2);
                 if(!Strings.isNullOrEmpty(tid) && !Strings.isNullOrEmpty(liverampSegment)) {
@@ -45,7 +49,8 @@ public class LoadUserDataController {
             e.printStackTrace();
         }
 
-        Iterable<UserSegmentData> all = userSegmentRepo.findAll();
+        List<UserSegmentData> all = Lists.newArrayList(userSegmentRepo.findAll());
+        logger.info("num rows: " + count + "  num records: " + all.size());
         all.forEach(x -> logger.info(x.toString()));
 
     }
--- src/main/java/com/taboola/tables/db/UserSegmentData.java
+++ src/main/java/com/taboola/tables/db/UserSegmentData.java
@@ -1,6 +1,8 @@
 package com.taboola.tables.db;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /**
@@ -8,7 +10,17 @@ import javax.persistence.Id;
  */
 @Entity
 public class UserSegmentData {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String tid;
     private String segments;
 
@@ -34,12 +46,38 @@ public class UserSegmentData {
         return segments;
     }
 
-    UserSegmentData() {
-    }
-
 
     public void setSegments(String segments) {
         this.segments = segments;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserSegmentData)) return false;
+
+        UserSegmentData that = (UserSegmentData) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (tid != null ? !tid.equals(that.tid) : that.tid != null) return false;
+        return segments != null ? segments.equals(that.segments) : that.segments == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (tid != null ? tid.hashCode() : 0);
+        result = 31 * result + (segments != null ? segments.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "UserSegmentData{" +
+                "id=" + id +
+                ", tid='" + tid + '\'' +
+                ", segments='" + segments + '\'' +
+                '}';
+    }
 }
